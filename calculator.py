cd3e2fd1bcd2633148990fc030eafb0bbe77a501
Mike Allan Nillo <nilloallan@gmail.com>
2020-06-20 23:04:35 +0800

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator library consisting basic math operations
 """
 
+
 def add(first_item, second_item):
     return first_item+second_item
+
 
 def subtract(first_item, second_item):
     return first_item-second_item
